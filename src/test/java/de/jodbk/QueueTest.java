e791210db41229149ff072dd40e8860e8e28966c
jobdk <johnybudnik+git@gmail.com>
2021-05-06 14:51:20 +0200

    added further tests

--- src/test/java/de/jodbk/QueueTest.java
+++ src/test/java/de/jodbk/QueueTest.java
@@ -3,6 +3,8 @@ package de.jodbk;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class QueueTest {
 
     private Queue queue;
@@ -16,5 +18,26 @@ public class QueueTest {
     public void test_dequeue_returns_IllegalStateException() {
         queue.dequeue();
     }
+
+
+    @Test
+    public void test_dequeue_returns_3() {
+        Queue queue = new Queue(2);
+        queue.enqueue(3);
+        queue.enqueue(2);
+
+        assertEquals("", 3, queue.dequeue());
+    }
+
+    @Test
+    public void test_dequeue_replacesLastAdded() {
+        Queue queue = new Queue(2);
+        queue.enqueue(3);
+        queue.enqueue(2);
+        queue.enqueue(4);
+
+        assertEquals("The last added number should be replaced", 4, queue.queue[queue.queue.length - 1]);
+    }
+
 
 }
--- src/test/java/de/jodbk/UtilTest.java
+++ src/test/java/de/jodbk/UtilTest.java
@@ -6,17 +6,46 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class UtilTest {
+
+
+    @Test(expected = IllegalArgumentException.class)
+    public void test_istErstesHalbjahr_returns_IllegalStateException_with_0() {
+        Util.istErstesHalbjahr(0);
+    }
+
+
     @Test
-    public void test_istErstesHalbjahr_returns_true() {
+    public void test_istErstesHalbjahr_returns_true_with_1() {
         boolean result = Util.istErstesHalbjahr(1);
 
         assertTrue("should be true", result);
     }
 
+
     @Test
-    public void test_istErstesHalbjahr_returns_false() {
+    public void test_istErstesHalbjahr_returns_true_with_6() {
+        boolean result = Util.istErstesHalbjahr(6);
+
+        assertTrue("should be true", result);
+    }
+
+
+    @Test
+    public void test_istErstesHalbjahr_returns_false_with_7() {
         boolean result = Util.istErstesHalbjahr(7);
 
         assertFalse("should be false", result);
+    }
+
+    @Test
+    public void test_istErstesHalbjahr_returns_false_with_12() {
+        boolean result = Util.istErstesHalbjahr(12);
+
+        assertFalse("should be false", result);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void test_istErstesHalbjahr_returns_IllegalStateException_with_13() {
+        Util.istErstesHalbjahr(13);
     }
 }
